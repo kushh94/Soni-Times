5597da877d3e02385e474baabeebd97c856a8af2
kushh94 <131980072+kushh94@users.noreply.github.com>
2025-09-10 22:32:36 +0530

    Updated unzip.js

--- unzip.js
+++ unzip.js
@@ -1,5 +1,6 @@
-{
-  "dependencies": {
-    "adm-zip": "^0.5.16"
-  }
-}
+const fs = require("fs");
+const unzipper = require("unzipper");
+
+fs.createReadStream("Soni Times Mobile App UI (4).zip")
+  .pipe(unzipper.Extract({ path: "SoniTimesUI" }))
+  .on("close", () => console.log("✅ Extraction complete"));
